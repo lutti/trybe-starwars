07755a2fd8bd6aa8d72ebb74f7e113c97f8f8b82
Lutti <lutti.c.amaral@gmail.com>
2022-09-28 00:12:22 -0300

    Provider

---
+++ src/context/Provider.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import MyContext from './MyContext';
+
+function Provider({ children }) {
+  const [stateA, setStateA] = useState('initialStateA');
+  const [stateB, setStateB] = useState('initialStateB');
+  const contextValue = {
+    stateA,
+    setStateA,
+    stateB,
+    setStateB,
+  };
+
+  return (
+    <MyContext.Provider value={ contextValue }>
+      {children}
+    </MyContext.Provider>
+  );
+}
+
+Provider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Provider;
