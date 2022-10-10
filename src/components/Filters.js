f8923a39399445e785b8d762b9bff2a4ead790a1
Lutti <lutti.c.amaral@gmail.com>
2022-10-09 21:21:15 -0300

    Requisito 7

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -68,40 +68,85 @@ function Filters() {
     // setBuscado(true);
   };
 
+  const clickDeleteFilter = (coluna) => {
+    // data = tableData;
+    setBuscado(false);
+    setFilterNumeric(filterByNumeric.filter((fil) => fil.coluna !== coluna));
+    setColumOptions(columOptions.concat(coluna));
+  };
+
+  const clickDeleteAllFilters = () => {
+    setBuscado(false);
+    setFilterNumeric([]);
+    setColumOptions(COLUNA_IS);
+  };
+
   return (
-    <div>
-      <select
-        name="coluna"
-        value={ filter.coluna }
-        onChange={ ({ target }) => setFilter({ ...filter, coluna: target.value }) }
-        data-testid="column-filter"
-      >
+    <>
+      <div>
+        <select
+          name="coluna"
+          value={ filter.coluna }
+          onChange={ ({ target }) => setFilter({ ...filter, coluna: target.value }) }
+          data-testid="column-filter"
+        >
+          {
+            columOptions.map((coluna) => (
+              <option key={ coluna } value={ coluna }>{ coluna }</option>
+            ))
+          }
+        </select>
+        <select
+          name="operador"
+          value={ filter.operador }
+          onChange={ ({ target }) => setFilter({ ...filter, operador: target.value }) }
+          data-testid="comparison-filter"
+        >
+          <option value="menor que">menor que</option>
+          <option value="maior que">maior que</option>
+          <option value="igual a">igual a</option>
+        </select>
+        <input
+          type="number"
+          value={ filter.number }
+          onChange={ ({ target }) => setFilter({ ...filter, number: target.value }) }
+          data-testid="value-filter"
+        />
+        <button type="button" onClick={ clickFilter } data-testid="button-filter">
+          Filtrar
+        </button>
+        <button
+          type="button"
+          onClick={ clickDeleteAllFilters }
+          data-testid="button-remove-filters"
+        >
+          Remover Todos Filtros
+        </button>
+      </div>
+      <div>
         {
-          columOptions.map((coluna) => (
-            <option key={ coluna } value={ coluna }>{ coluna }</option>
+          filterByNumeric.map((filtro) => (
+            <div
+              key={ `fil-${filtro.coluna}` }
+              id={ `fil-${filtro.coluna}` }
+              data-testid="filter"
+            >
+              <p>
+                { `${filtro.coluna} ${filtro.operador} ${filtro.number}` }
+              </p>
+              <button
+                type="button"
+                onClick={ ({ target }) => (
+                  clickDeleteFilter(target.parentElement.id.split('-')[1])
+                ) }
+              >
+                DELETE
+              </button>
+            </div>
           ))
         }
-      </select>
-      <select
-        name="operador"
-        value={ filter.operador }
-        onChange={ ({ target }) => setFilter({ ...filter, operador: target.value }) }
-        data-testid="comparison-filter"
-      >
-        <option value="menor que">menor que</option>
-        <option value="maior que">maior que</option>
-        <option value="igual a">igual a</option>
-      </select>
-      <input
-        type="number"
-        value={ filter.number }
-        onChange={ ({ target }) => setFilter({ ...filter, number: target.value }) }
-        data-testid="value-filter"
-      />
-      <button type="button" onClick={ clickFilter } data-testid="button-filter">
-        Filtrar
-      </button>
-    </div>
+      </div>
+    </>
   );
 }
 
